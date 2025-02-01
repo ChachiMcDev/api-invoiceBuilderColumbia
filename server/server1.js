cc24b0a9464a1558be0b289ecdee612370e5aa10
chuck at work <charlesnelson03@gmail.com>
2025-01-31 16:36:00 -0800

    changes to object structure on api flat file

--- server/server1.js
+++ server/server1.js
@@ -1,82 +1,129 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
-const cors = require('cors');
-
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());    // Enable CORS for all requests
 
-// app.use(cors({
-//     origin: 'http://192.168.1.5:9001' // Replace with your React app's origin
-// }));
+app.use(cors());
 
+// app.use(
+//   cors({
+//     origin: "http://10.17.5.12:9001", // Replace with your React app's origin
+//   })
+// );
 
 //test connection
-app.get('/api/test', async (req, res) => {
-    // res.setHeader('Access-Control-Allow-Methods', 'GET');
-    try {
-
-        const result = [{
-            _id: "12345",
-            invoiceNumber: 1,
-            description: "aaaaahhhhh yeah",
-            companyName: "Test Company",
-            address: "123 Test St",
-            city: "Test City",
-            state: "TS",
-            zip: "12345",
-            products: [
-                { itemName: 'Coors', itemPrice: 500 },
-                { itemName: 'Red Bull', itemPrice: 1500 },
-                { itemName: 'Mass', itemPrice: 150 }
-            ],
-            amount: 100,
-            createdAt: 123457
-        }, {
-            _id: "12345a",
-            invoiceNumber: 2,
-            description: "blahb lbha blah",
-            companyName: "Test Company2",
-            address: "244 Test St",
-            city: "Portland",
-            state: "TS",
-            zip: "98188",
-            products: [
-                { itemName: 'Red Drink', itemPrice: 500 },
-                { itemName: 'Blue Drink', itemPrice: 500 },
-                { itemName: 'Green Drink', itemPrice: 150 }
-            ],
-            amount: 5,
-            createdAt: 122456
-        }, {
-            _id: "12345c",
-            invoiceNumber: 2,
-            description: "ccccccccc",
-            companyName: "Test Company2",
-            address: "244 Test St",
-            city: "Portland",
-            state: "TS",
-            zip: "98188",
-            products: [
-                { itemName: 'Red Drink', itemPrice: 500 },
-                { itemName: 'Blue Drink', itemPrice: 500 },
-                { itemName: 'Green Drink', itemPrice: 150 }
-            ],
-            amount: 500,
-            createdAt: 122456
-        }];
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Bad Request" });
-    }
+app.get("/api/test", async (req, res) => {
+  // res.setHeader('Access-Control-Allow-Methods', 'GET');
+  try {
+    const result = [
+      {
+        _id: "1",
+        invoiceNumber: 1,
+        companyName: "Test Company",
+        address: "123 Test St",
+        city: "Test City",
+        state: "TS",
+        zip: "12345",
+        products: [
+          {
+            itemName: "koolaid",
+            itemPrice: 500,
+            deposit: 100,
+            discount: 50,
+            quantity: 5,
+          },
+          {
+            itemName: "soda",
+            itemPrice: 300,
+            deposit: 0,
+            discount: 0,
+            quantity: 20,
+          },
+          {
+            itemName: "green juice",
+            itemPrice: 500,
+            deposit: 0,
+            discount: 0,
+            quantity: 35,
+          },
+        ],
+      },
+      {
+        _id: "2",
+        invoiceNumber: 1,
+        companyName: "Test Company",
+        address: "123 Test St",
+        city: "Test City",
+        state: "TS",
+        zip: "12345",
+        products: [
+          {
+            itemName: "redbull",
+            itemPrice: 500,
+            deposit: 100,
+            discount: 50,
+            quantity: 5,
+          },
+          {
+            itemName: "cider",
+            itemPrice: 300,
+            deposit: 0,
+            discount: 0,
+            quantity: 20,
+          },
+          {
+            itemName: "ice tea",
+            itemPrice: 500,
+            deposit: 0,
+            discount: 0,
+            quantity: 35,
+          },
+        ],
+      },
+      {
+        _id: "3",
+        invoiceNumber: 1,
+        companyName: "Test Company",
+        address: "123 Test St",
+        city: "Test City",
+        state: "TS",
+        zip: "12345",
+        products: [
+          {
+            itemName: "maas",
+            itemPrice: 500,
+            deposit: 100,
+            discount: 50,
+            quantity: 5,
+          },
+          {
+            itemName: "beer",
+            itemPrice: 300,
+            deposit: 0,
+            discount: 0,
+            quantity: 20,
+          },
+          {
+            itemName: "wine",
+            itemPrice: 500,
+            deposit: 0,
+            discount: 0,
+            quantity: 35,
+          },
+        ],
+      },
+    ];
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Bad Request" });
+  }
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
